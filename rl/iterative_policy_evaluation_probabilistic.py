accd41cf9ac20fe614fa723bc004a0ee86a0f916
Timothy Wee <vn55kbe@m-dh6l17rdwf.lan>
2023-04-07 23:04:43 -0400

    DP done

--- rl/iterative_policy_evaluation_probabilistic.py
+++ rl/iterative_policy_evaluation_probabilistic.py
@@ -7,7 +7,7 @@ from builtins import range
 
 
 import numpy as np
-from grid_world import standard_grid, ACTION_SPACE
+from gridworld import windy_grid, ACTION_SPACE
 
 SMALL_ENOUGH = 1e-3  # threshold for convergence
 
@@ -39,6 +39,7 @@ if __name__ == '__main__':
   # the key is (s, a, s'), the value is the probability
   # that is, transition_probs[(s, a, s')] = p(s' | s, a)
   # any key NOT present will considered to be impossible (i.e. probability 0)
+  # we can take this from the grid object and convert it to the format we want
   transition_probs = {}
 
   # to reduce the dimensionality of the dictionary, we'll use deterministic
@@ -47,28 +48,23 @@ if __name__ == '__main__':
   # actually depend on (s, a)
   rewards = {}
 
-  grid = standard_grid()
-  for i in range(grid.rows):
-    for j in range(grid.cols):
-      s = (i, j)
-      if not grid.is_terminal(s):
-        for a in ACTION_SPACE:
-          s2 = grid.get_next_state(s, a)
-          transition_probs[(s, a, s2)] = 1
-          if s2 in grid.rewards:
-            rewards[(s, a, s2)] = grid.rewards[s2]
+  grid = windy_grid()
+  for (s, a), v in grid.probs.items():
+    for s2, p in v.items():
+      transition_probs[(s, a, s2)] = p
+      rewards[(s, a, s2)] = grid.rewards.get(s2, 0)
 
-  ### fixed policy ###
+  ### probabilistic policy ###
   policy = {
-      (2, 0): 'U',
-      (1, 0): 'U',
-      (0, 0): 'R',
-      (0, 1): 'R',
-      (0, 2): 'R',
-      (1, 2): 'U',
-      (2, 1): 'R',
-      (2, 2): 'U',
-      (2, 3): 'L',
+      (2, 0): {'U': 0.5, 'R': 0.5},
+      (1, 0): {'U': 1.0},
+      (0, 0): {'R': 1.0},
+      (0, 1): {'R': 1.0},
+      (0, 2): {'R': 1.0},
+      (1, 2): {'U': 1.0},
+      (2, 1): {'R': 1.0},
+      (2, 2): {'U': 1.0},
+      (2, 3): {'L': 1.0},
   }
   print_policy(policy, grid)
 
@@ -91,7 +87,7 @@ if __name__ == '__main__':
           for s2 in grid.all_states():
 
             # action probability is deterministic
-            action_prob = 1 if policy.get(s) == a else 0
+            action_prob = policy[s].get(a, 0)
 
             # reward is a function of (s, a, s'), 0 if not specified
             r = rewards.get((s, a, s2), 0)
@@ -108,4 +104,8 @@ if __name__ == '__main__':
 
     if biggest_change < SMALL_ENOUGH:
       break
+  print("V:", V)
   print("\n\n")
+
+  # sanity check
+  # at state (1, 2), value is 0.5 * 0.9 * 1 + 0.5 * (-1) = -0.05
