db01c5d2b38f9fe2ade2ea6a2f294162f56a146e
Tim Wee <tim.shawn@gmail.com>
2023-04-12 16:58:45 -0400

    rl2

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-NEW_PY_PATH = ${PWD}/rl:${PYTHONPATH}
+NEW_PY_PATH = ${PWD}/rl:${PWD}/rl2:${PWD}/rl3:${PYTHONPATH}
 
 notebook:
 	export PYTHONPATH="${NEW_PY_PATH}"; poetry run jupyter notebook
---
+++ rl2/cartpole/save_a_video.py
@@ -0,0 +1,68 @@
+# https://deeplearningcourses.com/c/deep-reinforcement-learning-in-python
+# https://www.udemy.com/deep-reinforcement-learning-in-python
+# Note: you may need to update your version of future
+# sudo pip install -U future
+
+import gymnasium as gym
+from gymnasium.wrappers import RecordVideo
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+
+def get_action(s, w):
+  return 1 if s.dot(w) > 0 else 0
+
+
+def play_one_episode(env, params):
+  observation = env.reset()[0]
+  done = False
+  t = 0
+
+  while not done and t < 10000:
+    t += 1
+    action = get_action(observation, params)
+    observation, reward, done, trunc, info = env.step(action)
+    if done:
+      break
+
+  return t
+
+
+def play_multiple_episodes(env, T, params):
+  episode_lengths = np.empty(T)
+
+  for i in range(T):
+    episode_lengths[i] = play_one_episode(env, params)
+
+  avg_length = episode_lengths.mean()
+  print("avg length:", avg_length)
+  return avg_length
+
+
+def random_search(env):
+  episode_lengths = []
+  best = 0
+  params = None
+  for t in range(100):
+    new_params = np.random.random(4)*2 - 1
+    avg_length = play_multiple_episodes(env, 100, new_params)
+    episode_lengths.append(avg_length)
+
+    if avg_length > best:
+      params = new_params
+      best = avg_length
+  return episode_lengths, params
+
+
+if __name__ == '__main__':
+  env = gym.make('CartPole-v1', render_mode="rgb_array")
+  episode_lengths, params = random_search(env)
+  #plt.plot(episode_lengths)
+  #plt.show()
+
+  # play a final set of episodes
+  # env = wrappers.Monitor(env, 'my_awesome_dir')
+  # env = wrappers.RecordVideo(env, 'my_awesome_dir')
+  env = RecordVideo(env, sys.argv[1])
+  print("***Final run with final weights***:", play_one_episode(env, params))
